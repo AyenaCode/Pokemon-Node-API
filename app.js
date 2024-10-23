a0949276e8723ea3c2ce346b2f813f43b5dd893a
AyenaCode <ayenacode1@gmail.com>
2024-10-23 16:25:45 -0400

    dynamique route

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const expresss = require("express");
 const app = expresss();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello express"));
+app.get("/", (req, res) => res.send("Hello express")); //endPoint sur la route principal
+
+app.get("/api/pokemon/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Vous avez demander le pokemon numero ${id}`);
+}); //seconde route
 
 app.listen(port, () =>
   console.log(`Notre appli est sur : http://localhost:${port}`)
