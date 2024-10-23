df18587ba6f95217957507254da97e47321f762f
AyenaCode <ayenacode1@gmail.com>
2024-10-23 16:51:14 -0400

    dinamic display

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const expresss = require("express");
+let pokemons = require("./mock-pokemon");
 
 const app = expresss();
 const port = 3000;
@@ -6,9 +7,10 @@ const port = 3000;
 app.get("/", (req, res) => res.send("Hello express")); //endPoint sur la route principal
 
 app.get("/api/pokemon/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(`Vous avez demander le pokemon numero ${id}`);
-}); //seconde route
+  const id = parseInt(req.params.id);
+  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+  res.send(`Vous avez demander le pokemon numero ${pokemon.name}`);
+});
 
 app.listen(port, () =>
   console.log(`Notre appli est sur : http://localhost:${port}`)
