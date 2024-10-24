b5348fde773c1baccb17532334a463df8d4f181b
AyenaCode <ayenacode1@gmail.com>
2024-10-24 04:18:43 -0400

    middleware

--- app.js
+++ app.js
@@ -5,19 +5,28 @@ const { success } = require("./_helper");
 const app = expresss();
 const port = 3000;
 
+//Middleware
+const logger = (req, res, next) => {
+  console.log(`Url : ${req.url}`);
+  next();
+};
+app.use(logger);
+
+//Route principale
 app.get("/", (req, res) => res.send("Hello express")); //endPoint sur la route principal
 
+//tous les pokemons
+app.get("/api/pokemons", (req, res) => {
+  const message = "La liste des pokemons a bien été trouvée";
+  res.json(success(message, pokemons));
+});
+
+//Un seul pokemon
 app.get("/api/pokemon/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const pokemon = pokemons.find((pokemon) => pokemon.id === id);
   const message = "Un pokemon  été trouver";
   res.json(success(message, pokemon));
-});
-
-//tous les
-app.get("/api/pokemons", (req, res) => {
-  const message = "La liste des pokemons a bien été trouvée";
-  res.json(success(message, pokemons));
 });
 
 //Lancer le server
